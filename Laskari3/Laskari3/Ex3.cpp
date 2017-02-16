b5839cc095cb29c8a43147a2bb7ec3e8bb8fab37
sofiev <sofiev@localhost.localdomain>
2017-02-16 17:45:28 +0200

    2016602161745

--- Laskari3/Laskari3/Ex3.cpp
+++ Laskari3/Laskari3/Ex3.cpp
@@ -50,12 +50,13 @@ int Ex3(){
         //cout << double(iHit) << " "<<double(iHit+iMiss) <<endl;
         dTemp =0.5*10.0*double(iHit)/double(iHit+iMiss) -dExact;
 
+        cout << "N=10^"<< i <<", ";
         printf("delta I: %.9f ,", dTemp);
         printf("Time: %8.9f sec \n",difftime(t2,t1)/CLOCKS_PER_SEC);
 
 	}
 
-	// Hit and miss
+	// DS
 	cout << "DS" <<endl;
 	double dSum=0.0;
 	for(int i=2; i<7; i++){
@@ -72,10 +73,83 @@ int Ex3(){
         //cout << double(iHit) << " "<<double(iHit+iMiss) <<endl;
         dTemp =dSum*10.0/double(N) -dExact;
 
+        cout << "N=10^"<< i <<", ";
         printf("delta I: %.9f ,", dTemp);
         printf("Time: %8.9f sec \n",difftime(t2,t1)/CLOCKS_PER_SEC);
 
 	}
+
+
+	// SS
+	cout << "SS" <<endl;
+
+	for(int i=2; i<7; i++){
+		t1 = clock();
+		iHit=0; iMiss=0;
+		N=pow(10,i);
+		dSum=0.0;
+		for(int iDx=0;iDx<int(sqrt(N)); iDx++){
+            for(int iDy=0;iDy<int(sqrt(N)); iDy++){
+                dCoord1=iDx*10.0 /int(sqrt(N))+genrand64_real3()*10.0 /int(sqrt(N));
+                dCoord2=genrand64_real3()*0.5 /int(sqrt(N))+ iDy*0.5 /int(sqrt(N));
+                if(dCoord2<fEx3Function1(dCoord1, dLambda)){
+                    iHit++;
+                }else{
+                    iMiss++;
+                }
+
+            }
+		}
+
+        t2 = clock();
+
+        //cout << double(iHit) << " "<<double(iHit+iMiss) <<endl;
+        dTemp =double(iHit)/double(iHit+iMiss) *10.0*0.5 -dExact;
+
+        cout << "N=10^"<< i <<", ";
+        printf("delta I: %.9f ,", dTemp);
+        printf("Time: %8.9f sec \n",difftime(t2,t1)/CLOCKS_PER_SEC);
+
+	}
+
+
+
+
+	// PSS
+	cout << "PSS" <<endl;
+
+	for(int i=2; i<7; i++){
+		t1 = clock();
+		iHit=0; iMiss=0;
+		N=pow(10,i);
+		dSum=0.0;
+		for(int iDx=0;iDx<100; iDx++){
+                dCoord1=iDx*10.0 /100.0+genrand64_real3()*10.0 /100.0;
+
+                for(int iRand=0; iRand<N/100    ; iRand++){
+                    dCoord2=genrand64_real3()*0.5;
+                    if(dCoord2<fEx3Function1(dCoord1, dLambda)){
+                        iHit++;
+                    }else{
+                        iMiss++;
+                    }
+                }
+            }
+
+        t2 = clock();
+
+        //cout << double(iHit) << " "<<double(iHit+iMiss) <<endl;
+        dTemp =double(iHit)/double(iHit+iMiss) *10.0*0.5 -dExact;
+
+        cout << "N=10^"<< i <<", ";
+        printf("delta I: %.9f ,", dTemp);
+        printf("Time: %8.9f sec \n",difftime(t2,t1)/CLOCKS_PER_SEC);
+
+
+	}
+
+
+
 
 
 //	printf("Time: %8.5f sec", difftime(t2,t1)/CLOCKS_PER_SEC)
