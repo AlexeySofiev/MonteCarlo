ca0d7d4920a99fbeaf8741e95338e675eb4ccebc
sofiev <sofiev@localhost.localdomain>
2017-02-28 14:39:56 +0200

    201702281439

---
+++ Laskari4/Laskari4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "randoms.h"
+
+using namespace std;
+void init_genrand64(unsigned long long seed);
+double genrand64_real3(void);
+int Ex1();
+int test();
+
+int main()
+{
+    Ex1();
+    //test();
+    return 0;
+}
